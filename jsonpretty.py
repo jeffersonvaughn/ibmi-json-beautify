e48316ad9763f00dc25229286e36d76b8bbb3888
Jefferson Vaughn <35352921+jeffersonvaughn@users.noreply.github.com>
2022-02-01 10:40:37 -0500

    Update jsonpretty.py

--- jsonpretty.py
+++ jsonpretty.py
@@ -1,7 +1,10 @@
 import json
 import sys
 
-with open(sys.argv[1], "r") as read_file:
+with open(sys.argv[2], "r") as read_file:
     g_jsonData = json.load(read_file)
-    PrettyJson = json.dumps(g_jsonData, indent=4, separators=(',', ': '), sort_keys=True)
+    if int(sys.argv[1]) > 0:
+      PrettyJson = json.dumps(g_jsonData, indent=int(sys.argv[1]), separators=(',', ': '), sort_keys=True)
+    else:
+      PrettyJson = json.dumps(g_jsonData)
     print(PrettyJson)
